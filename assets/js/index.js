f00c457becb844a5f5adcf4f1c957210d46f146a
Alexandra Batrak <4528953+alexandrabatrak@users.noreply.github.com>
2023-01-27 21:23:07 +0000

    Display only today's tasks

--- assets/js/index.js
+++ assets/js/index.js
@@ -18,7 +18,7 @@
   }
 
   let taskData = JSON.parse(localStorage.getItem('taskData')) || [];
-
+  let today = moment().format('LL');
   // rended blocks
   for (let i = 0; i < blocks.length; i++) {
     $('.container').append(
@@ -26,9 +26,9 @@
           <div class="hour col-2 d-flex justify-content-left pt-2">
             <span id="block-time" class="" data-index="${i}">${blocks[i]}</span>
           </div>
-          <div class="col-9 d-flex w-100">
+          <div class="col-9 d-flex w-100 p-0">
             <input id="daily-task" class="w-100 px-2" value="${
-              taskData[i] ? taskData[i].task : ''
+              taskData[i] && taskData[i].date === today ? taskData[i].task : ''
             }" data-index="${i}"/>
           </div>
           <div class="col-1 d-flex p-0 w-100 h-100">
@@ -46,15 +46,18 @@
     let i = $(this).data('index');
     let task = $(`input[data-index=${i}]`).val();
     console.log(task);
+    let date = moment().format('LL');
 
     if (!taskData[i]) {
       taskData[i] = {
         blockTime: blocks[i],
         task: task,
+        date: date,
       };
     } else {
       taskData[i].blockTime = blocks[i];
       taskData[i].task = task;
+      taskData[i].date = date;
     }
     localStorage.setItem('taskData', JSON.stringify(taskData));
   });
@@ -70,25 +73,13 @@
     row.each(function () {
       // convert block time string to a moment object
       let blockTime = moment($(this).find('#block-time').text(), 'HH').hour();
-
+      let input = $(this).children().eq(1);
       if (blockTime < now) {
-        $(this)
-          .children()
-          .eq(1)
-          .removeClass(['present', 'future'])
-          .addClass('past');
+        input.removeClass(['present', 'future']).addClass('past');
       } else if (blockTime === now) {
-        $(this)
-          .children()
-          .eq(1)
-          .removeClass(['past', 'future'])
-          .addClass('present');
+        input.removeClass(['past', 'future']).addClass('present');
       } else {
-        $(this)
-          .children()
-          .eq(1)
-          .removeClass(['past', 'present'])
-          .addClass('future');
+        input.removeClass(['past', 'present']).addClass('future');
       }
     });
     setInterval(colorise, 60000);
