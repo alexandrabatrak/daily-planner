5677f1b42cd638696a1d85bfe386060cc9a6e0e0
Alexandra Batrak <4528953+alexandrabatrak@users.noreply.github.com>
2023-01-28 01:21:36 +0000

    Add icon animation

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,88 +1,106 @@
 (function ($) {
   // header time
-  let updateTime = () => {
-    $('#currentDay').text(moment().format('LLL'));
-    setInterval(updateTime, 60000);
-  };
-  setTimeout(updateTime, 0);
+  $('#currentDay').text(moment().format('LL'));
+  // let updateTime = () => {
+  //   $('#currentTime').text(moment().format('HH:mm'));
+  //   setInterval(updateTime, 60000);
+  // };
+  // setTimeout(updateTime, 0);
 
   // populate time blocks with save buttons
   let blocks = [];
   let hours = {
     start: 9,
     end: 17,
-    window: 1,
   };
+  // hourly
   for (let i = hours.start; i <= hours.end; i++) {
-    blocks.push(`${i}:00`);
+    blocks.push(moment().hour(i).format('h A'));
   }
 
   let taskData = JSON.parse(localStorage.getItem('taskData')) || [];
   let today = moment().format('LL');
   // rended blocks
   for (let i = 0; i < blocks.length; i++) {
-    $('.container').append(
+    $('#container').append(
       `<div class="row w-100" id="row-${i}">
-          <div class="hour col-2 d-flex justify-content-left pt-2">
+          <div class="hour col-2 d-flex justify-content-left align-items-center">
             <span id="block-time" class="" data-index="${i}">${blocks[i]}</span>
           </div>
-          <div class="col-9 d-flex w-100 p-0">
+          <div class="col-md-9 col-8 d-flex w-100 p-0">
             <input id="daily-task" class="w-100 px-2" value="${
               taskData[i] && taskData[i].date === today ? taskData[i].task : ''
             }" data-index="${i}"/>
           </div>
-          <div class="col-1 d-flex p-0 w-100 h-100">
+          <div class="col-md-1 col-2 d-flex p-0 w-100 h-100">
             <button id="save-button" class="btn save-button d-flex align-items-center justify-content-center w-100 h-100" data-index="${i}">
-              <i class="fa fa-save"></i>
+            <i class="fa-solid fa-plus"></i>
             </button>
           </div>
-      <div>
-    `
+      <div>`
     );
+
+    // event listener for save
+    // moved in to see if it will work instead of creating listener separately
+    // *event delegation*
+    $('#container').on('click', '#save-button', function () {
+      let i = $(this).data('index');
+      let task = $(`input[data-index=${i}]`).val();
+      let date = moment().format('LL');
+
+      if (!taskData[i]) {
+        taskData[i] = {
+          blockTime: blocks[i],
+          task: task,
+          date: date,
+        };
+      } else {
+        taskData[i].blockTime = blocks[i];
+        taskData[i].task = task;
+        taskData[i].date = date;
+      }
+      localStorage.setItem('taskData', JSON.stringify(taskData));
+      $(this).children('i').removeClass('fa-plus').addClass('fa-check saved');
+      setTimeout(() => {
+        $(this).children('i').removeClass('fa-check saved').addClass('fa-plus');
+      }, 1000);
+    });
   }
-
-  // event listener for save
-  $('.container').on('click', '#save-button', function () {
-    let i = $(this).data('index');
-    let task = $(`input[data-index=${i}]`).val();
-    console.log(task);
-    let date = moment().format('LL');
-
-    if (!taskData[i]) {
-      taskData[i] = {
-        blockTime: blocks[i],
-        task: task,
-        date: date,
-      };
-    } else {
-      taskData[i].blockTime = blocks[i];
-      taskData[i].task = task;
-      taskData[i].date = date;
-    }
-    localStorage.setItem('taskData', JSON.stringify(taskData));
-  });
 
   // colorise
   function colorise() {
     // get current time
-    // let now = moment().format('HH:mm');
-    let nowTime = '11:48:35';
-    let now = moment(nowTime, 'HH:mm:ss').hour();
+    // let nowTime = moment().format('HH:mm');
+    let nowTime = 'January 28th 2023, 3:48:35';
+    let now = moment(nowTime, 'MMMM Do YYYY, h:mm:ss a').hour();
 
-    let row = $('.row');
-    row.each(function () {
+    $('.row').each(function () {
       // convert block time string to a moment object
       let blockTime = moment($(this).find('#block-time').text(), 'HH').hour();
-      let input = $(this).children().eq(1);
+      let row = $(this).children();
       if (blockTime < now) {
-        input.removeClass(['present', 'future']).addClass('past');
+        row.removeClass(['present', 'future']).addClass('past');
       } else if (blockTime === now) {
-        input.removeClass(['past', 'future']).addClass('present');
+        row.removeClass(['past', 'future']).addClass('present');
       } else {
-        input.removeClass(['past', 'present']).addClass('future');
+        row.removeClass(['past', 'present']).addClass('future');
       }
     });
     setInterval(colorise, 60000);
   }
   setTimeout(colorise, 0);
+
+  // main height
+  let height = () => {
+    $('#container').css({
+      'min-height': `calc( 100vh - ${
+        $('header').outerHeight() + $('footer').outerHeight()
+      }px )`,
+    });
+  };
+  height();
+
+  window.onresize = function () {
+    height();
+  };
 })(jQuery);
