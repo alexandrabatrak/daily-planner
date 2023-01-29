356c500373d57ed96f6f4a78f6ad123f14786e60
Alexandra Batrak <4528953+alexandrabatrak@users.noreply.github.com>
2023-01-29 03:21:31 +0000

    Add user selection for hours

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,54 +11,51 @@
   //
   let blocks = [];
   let hours = {
-    start: 8,
+    start: 02,
     end: 23,
   };
-  // hourly
-  for (let i = hours.start; i <= hours.end; i++) {
-    blocks.push(moment().hour(i).format('h A'));
+  if (localStorage.getItem('hours')) {
+    hours = JSON.parse(localStorage.getItem('hours'));
   }
-  // 30 min
-  // *doesn't work as intended. If using this would need to update the colorise() as well
-  // for (let i = hours.start; i <= hours.end; i += 0.5) {
-  //   blocks.push(moment().hour(i).startOf('hour').format('h:mm A'));
-  // }
+  // get user selected hours
+  const storedHours = JSON.parse(localStorage.getItem('hours'));
+  let hoursStart = $('<input type="text" id="user-hours-start" value="2"/>');
+  let hoursEnd = $('<input type="text" id="user-hours-end" value="23"/>');
+  hoursStart.val(storedHours.start);
+  hoursEnd.val(storedHours.end);
+  let submitHours = $(
+    '<button id="submitHours" class="btn"><i class="fa-solid fa-check"></i></button>'
+  );
+  $('.time-wrapper').after(`<div class="hours-input"></div>`);
+  $('.hours-input').append([hoursStart, hoursEnd, submitHours]);
 
-  console.log(blocks);
+  submitHours.on('click', (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    blocks = [];
+    $('#container').empty();
+    hours.start = moment(hoursStart.val(), 'hh').format('HH');
+    hours.end = moment(hoursEnd.val(), 'hh').format('HH');
+    localStorage.setItem('hours', JSON.stringify(hours));
+    renderBlocks();
+    colorise();
+  });
 
   let taskData = JSON.parse(localStorage.getItem('taskData')) || [];
-  let today = moment().format('LL');
-  // render blocks
-  // for (let i = 0; i < blocks.length; i++) {
-  //   $('#container').append(
-  //     `<div class="row w-100" id="row-${i}">
-  //       <div class="hour col-2 d-flex justify-content-left align-items-center">
-  //       <span id="block-time" class="" data-index="${i}">${blocks[i]}</span>
-  //       </div>
-  //       <div class="col-md-9 col-8 d-flex w-100 p-0">
-  //       <input id="daily-task" class="w-100 px-2" value="${
-  //         taskData[i] && taskData[i].date === today ? taskData[i].task : ''
-  //       }" data-index="${i}"/>
-  //       </div>
-  //       <div class="col-md-1 col-2 d-flex p-0 w-100 h-100">
-  //       <button id="save-button" class="btn save-button d-flex align-items-center justify-content-center w-100 h-100" data-index="${i}">
-  //       <i class="fa-solid fa-plus"></i>
-  //       </button>
-  //       </div>
-  //       <div>`
-  //   );
-
-  for (let i = 0; i < blocks.length; i++) {
-    // check if row exists to avoid creating empty div inside on each loop
-    let row = $(`#row-${i}`);
-    // if row exists update input value from the localStorage
-    if (row.length) {
-      let input = $(`#daily-task[data-index=${i}]`);
-      input.val(
-        taskData[i] && taskData[i].date === today ? taskData[i].task : ''
-      );
-    } else {
-      // create new row and do all that fancy shmancy stuff
+  function renderBlocks() {
+    // save all button
+    $('#container').append(
+      `<div class="row w-100 d-flex justify-content-end align-items-center">
+            <button id="ultimate-save" class="btn text-uppercase">Save all</button>
+          </div>`
+    );
+    for (let i = hours.start; i <= hours.end; i++) {
+      blocks.push(moment().hour(i).format('h A'));
+    }
+    let taskData = JSON.parse(localStorage.getItem('taskData')) || [];
+    let today = moment().format('LL');
+    // render blocks
+    for (let i = 0; i < blocks.length; i++) {
       $('#container').append(
         `<div class="row w-100" id="row-${i}">
                   <div class="hour col-2 d-flex justify-content-left align-items-center">
@@ -80,35 +77,24 @@
                   </div>
               </div>`
       );
+      // }
+      // event listener for save
+      // moved in to see if it will work instead of creating listener separately
+      // *event delegation*
+
+      $('#container').on('click', '#save-button', saveTask);
+      $('#container').on('keyup', 'input[id="daily-task"]', function (e) {
+        // on pressing enter from input, call function passing 'this' arguments
+        if (e.which === 13) {
+          e.stopPropagation();
+          e.preventDefault();
+          // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/call
+          saveTask.call(this);
+        }
+      });
     }
-
-    // event listener for save
-    // moved in to see if it will work instead of creating listener separately
-    // *event delegation*
-    // $('#container').on('click', '#save-button', function () {
-    //   // animate icon
-    //   $(this).children('i').removeClass('fa-plus').addClass('fa-check saved');
-    //   let i = $(this).data('index');
-    //   let task = $(`input[data-index=${i}]`).val();
-
-    //   saveToLocal(i, task);
-    //   // return icon to default
-    //   setTimeout(() => {
-    //     $(this).children('i').removeClass('fa-check saved').addClass('fa-plus');
-    //   }, 1000);
-    // });
-    $('#container').on('click', '#save-button', saveTask);
-    $('#container').on('keyup', 'input[id="daily-task"]', function (e) {
-      // on pressing enter from input, call function passing 'this' arguments
-      if (e.which === 13) {
-        e.stopPropagation();
-        e.preventDefault();
-        console.log(e.which);
-        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/call
-        saveTask.call(this);
-      }
-    });
   }
+  renderBlocks();
 
   // event listener callback
   function saveTask() {
@@ -125,13 +111,7 @@
     }, 1000);
   }
 
-  // save all button
-  $('#container').append(
-    `<div class="row w-100 d-flex justify-content-end align-items-center">
-      <button id="ultimate-save" class="btn text-uppercase">Save all</button>
-    </div>`
-  );
-  $('#container').on('click', '#ultimate-save', function () {
+  $('main').on('click', '#ultimate-save', function () {
     let saveAllBtn = $('#ultimate-save');
     saveAllBtn.prop('disabled', true).text('Wait...');
     $(`input[id='daily-task']`).each(function () {
@@ -165,29 +145,32 @@
     localStorage.setItem('taskData', JSON.stringify(taskData));
   }
 
-  let nowTime = moment();
-
-  let testTime = $(
-    '<button id="testTime" class="btn">Set time to 1:40PM to test</button>'
-  );
-  $('.time-wrapper').append(testTime);
+  // let testTime = $(
+  //   `<button id="testTime" class="btn">Set time to 1:40PM to test</button>`
+  // );
+  // $('.time-wrapper').append(testTime);
 
   // testing testing who is who
   // TODO: Add a button for test date - randomise it within the range - for testing outside of business hours
   // *This works, apart from input field is not getting 'past' class removed
-  testTime.on('click', setTestTime);
-  function setTestTime() {
-    nowTime = moment('13:40', 'HH:mm');
-    console.log(nowTime);
-    colorise(nowTime);
-  }
+  // nah it doesn't work anymore fuck this shit.
 
   // colorise
-  function colorise(nowTime) {
-    // get current time
+  // let testing = false;
+  // testTime.on('click', () => {
+  // testing = true;
+  // colorise();
+  // });
 
-    now = moment(nowTime).format('HH');
-    console.log(now);
+  function colorise() {
+    // let nowTime;
+    // if (testing) {
+    //   nowTime = moment('13:40', 'HH:mm');
+    // } else {
+    //   nowTime = moment();
+    // }
+    let now = moment().format('HH');
+    // get current time
 
     $('.row').each(function () {
       // convert block time string to a moment object
@@ -195,20 +178,24 @@
         'HH'
       );
       let row = $(this).children();
+      row.removeClass(['past', 'present', 'future']);
       if (blockTime < now) {
         // moment.js functions are not working properly??
-        // if (moment(blockTime).isBefore(nowTime)) {
-        row.removeClass(['present', 'future']).addClass('past');
+        // if (moment(blockTime).isBefore(now)) {
+        // row.removeClass(['present', 'future'])
+        row.addClass('past');
       } else if (blockTime === now) {
-        // else if (moment(blockTime).isSame(nowTime)) {
-        row.removeClass(['past', 'future']).addClass('present');
+        // else if (moment(blockTime).isSame(now)) {
+        // row.removeClass(['past', 'future'])
+        row.addClass('present');
       } else {
-        row.removeClass(['past', 'present']).addClass('future');
+        // row.removeClass(['past', 'present'])
+        row.addClass('future');
       }
     });
-    setInterval(() => colorise(nowTime), 60000);
+    // setInterval(() => colorise(), 60000);
   }
-  setTimeout(() => colorise(nowTime), 0);
+  setTimeout(() => colorise(), 0);
 
   // main height
   let height = () => {
